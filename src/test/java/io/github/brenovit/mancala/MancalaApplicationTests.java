7051597f84061146922a1f48a0a4fcea88465db2
Breno Nunes <bnunes@gap.com>
2021-12-05 04:19:57 -0800

    setup spring

---
+++ src/test/java/io/github/brenovit/mancala/MancalaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brenovit.mancala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MancalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

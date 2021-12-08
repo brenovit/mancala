01f12e185c41ed6a7eff92f81225fac5a55d01c4
Breno Arantes <breno.arantes97@gmail.com>
2021-12-08 14:49:47 -0300

    Fix About it page

--- src/main/resources/static/js/view/general/about.vue.js
+++ src/main/resources/static/js/view/general/about.vue.js
@@ -1,10 +1,10 @@
 const About = {
     template: `
-    <h1> About id</h1>
+    <h1> About it</h1>
     <div>
-    <p>Board design <a href="https://codepen.io/ChiliTomatoNoodle">Mancala Board</a> from <a href="https://codepen.io/ChiliTomatoNoodle">@ChiliTomatoNoodle</a></p>
-    <p>Logo <a href="https://thenounproject.com/icon/stone-games-3312354/">stone games</a> from <a href="https://thenounproject.com/shmai.com/">Azam Ishaq</a></p>
-    <p>Game logic <a href="https://github.com/brenovit/mancala">brenovit</a></p>
+    <p>Board design <a target="_blank" href="https://codepen.io/ChiliTomatoNoodle/pen/LOaPmy">Mancala Board</a> from <a target="_blank" href="https://codepen.io/ChiliTomatoNoodle">@ChiliTomatoNoodle</a></p>
+    <p>Logo <a target="_blank" href="https://thenounproject.com/icon/stone-games-3312354/">stone games</a> from <a target="_blank" href="https://thenounproject.com/shmai.com/">Azam Ishaq</a></p>
+    <p>Game logic <a target="_blank" href="https://github.com/brenovit/mancala">brenovit</a></p>
     </div>
     `,
     data() {

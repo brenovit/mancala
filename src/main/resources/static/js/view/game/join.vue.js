54935faa98d0bda8f93390f55c5216893e081e2e
Breno Arantes <breno.arantes97@gmail.com>
2021-12-08 22:55:48 -0300

    Refactor join game page
    - Add index column
    - Change headder color
    - Remove game list page

--- src/main/resources/static/js/view/game/join.vue.js
+++ src/main/resources/static/js/view/game/join.vue.js
@@ -1,34 +1,153 @@
 const JoinGame = {
-    template: `
-    <div>
+    template: `    
     <table>
-  
-  <thead>
-    <th>#</th>
-    <th>player name</th>
-    <th>game name</th>
-    <th>game status</th>
-    <th>spectors</th>
-    <th>action</th>
-  </thead>
-  
-  <tbody>
-    <tr v-for="game in games" :key="game.id">
-      <td data-label="player name">{{game.id}}</td>  
-      <td data-label="player name">{{game.player.name}}</td>
-      <td data-label="game name">{{game.name}}</td>
-      <td data-label="game status">{{game.status}}</td>
-      <td data-label="spectors">{{game.spectors}}</td>
-      <td data-label="action"><button @click="joinGame(game.id)">Join</button>|<button @click="watchGame(game.id)">Watch</button></td>
-    </tr>
-  </tbody>
-  
-</table>
-    </div>    
+        <thead>
+            <th>#</th>
+            <th>player name</th>
+            <th>game id</th>
+            <th>game name</th>
+            <th>game status</th>
+            <th>spectors</th>
+            <th>action</th>
+        </thead>
+
+        <tbody>
+            <tr v-for="(game,index) in games" :key="game.id">
+            <td data-label="player name">{{index}}</td>
+            <td data-label="player name">{{game.id}}</td>
+            <td data-label="player name">{{game.player.name}}</td>
+            <td data-label="game name">{{game.name}}</td>
+            <td data-label="game status">{{game.status}}</td>
+            <td data-label="spectors">{{game.spectors}}</td>
+            <td data-label="action"><button @click="joinGame(game.id)">Join</button>|<button @click="watchGame(game.id)">Watch</button></td>
+            </tr>
+        </tbody>
+
+    </table>
     `,
     data() {
             return {
                 games: [
+                    {
+                        id: 'gid1',
+                        name: 'test game',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid1',
+                            name: 'p 1',
+                        },
+                        spectors: 3
+                    },
+                    {
+                        id: 'gid2',
+                        name: 'test game 2',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid2',
+                            name: 'p 2',
+                        },
+                        spectors: 0
+                    },
+                    {
+                        id: 'gid3',
+                        name: 'test game 3',
+                        status: 'in progress',
+                        player: {
+                            id: 'pid3',
+                            name: 'p 3',
+                        },
+                        spectors: 9
+                    },
+                    {
+                        id: 'gid4',
+                        name: 'test game4',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid1',
+                            name: 'p 1',
+                        },
+                        spectors: 3
+                    },
+                    {
+                        id: 'gid5',
+                        name: 'test game 5',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid2',
+                            name: 'p 2',
+                        },
+                        spectors: 0
+                    },
+                    {
+                        id: 'gid6',
+                        name: 'test game 6',
+                        status: 'in progress',
+                        player: {
+                            id: 'pid3',
+                            name: 'p 3',
+                        },
+                        spectors: 9
+                    },
+                    {
+                        id: 'gid1',
+                        name: 'test game',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid1',
+                            name: 'p 1',
+                        },
+                        spectors: 3
+                    },
+                    {
+                        id: 'gid2',
+                        name: 'test game 2',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid2',
+                            name: 'p 2',
+                        },
+                        spectors: 0
+                    },
+                    {
+                        id: 'gid3',
+                        name: 'test game 3',
+                        status: 'in progress',
+                        player: {
+                            id: 'pid3',
+                            name: 'p 3',
+                        },
+                        spectors: 9
+                    },
+                    {
+                        id: 'gid4',
+                        name: 'test game4',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid1',
+                            name: 'p 1',
+                        },
+                        spectors: 3
+                    },
+                    {
+                        id: 'gid5',
+                        name: 'test game 5',
+                        status: 'waiting',
+                        player: {
+                            id: 'pid2',
+                            name: 'p 2',
+                        },
+                        spectors: 0
+                    },
+                    {
+                        id: 'gid6',
+                        name: 'test game 6',
+                        status: 'in progress',
+                        player: {
+                            id: 'pid3',
+                            name: 'p 3',
+                        },
+                        spectors: 9
+                    },
                     {
                         id: 'gid1',
                         name: 'test game',
